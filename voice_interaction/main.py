fde1223db087d3563c303d8a8d8f86be2988daf1
Justin <2518835772@qq.com>
2024-08-03 17:16:25 +0800

    语音播放动画

--- voice_interaction/main.py
+++ voice_interaction/main.py
@@ -1,9 +1,11 @@
 from main_agents.main_agents import open_agents
 from main_function import audio
+from interface.show_video import monitor_emotion_and_play
 
 
 def main():
     while True:
+        monitor_emotion_and_play()
         audio()
         open_agents()
 

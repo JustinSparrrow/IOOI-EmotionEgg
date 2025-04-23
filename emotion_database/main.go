d96963ba1f64af34a7f3d042a192e5686f5d3942
Justin <2518835772@qq.com>
2025-04-23 21:40:10 +0800

    update

--- emotion_database/main.go
+++ emotion_database/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	database.InitDB() // ✅ 添加这一行来初始化 DB 连接
+	database.InitDB() //
 	r := router.SetupRouter()
 	err := r.SetTrustedProxies([]string{"127.0.0.1"})
 	if err != nil {
 		return
-	} // 🟢 设置可信代理
+	} // 设置可信代理
 	err = r.Run(":8080")
 	if err != nil {
 		return
-	} // ✅ 必须有冒号
+	}
 }
